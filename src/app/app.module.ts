1e3e1f969dcbc6a42e267780d8564427beba8a05
hafsasam <34801393+hafsasam@users.noreply.github.com>
2018-07-03 16:16:13 +0100

    created first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { WhoRComponent } from './who-r/who-r.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ValuesSliderComponent } from './values-slider/values-slider.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    PresentationComponent,
+    WhoRComponent,
+    PartnersComponent,
+    ValuesSliderComponent,
+    ServicesComponent,
+    ProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
