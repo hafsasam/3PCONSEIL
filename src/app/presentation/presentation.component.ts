d3d48d4fd06f0ef5ea6d64c1d02530732057ca43
hafsasam <34801393+hafsasam@users.noreply.github.com>
2018-07-06 12:36:53 +0100

    added presentation & partners component

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./presentation.component.css']
 })
 export class PresentationComponent implements OnInit {
+  box1 = {
+      title: 'Notre force c’est surtout notre capital humain',
+      description: 'composé de compétences ayant un bon niveau d’expertise, une grande intégrité,' +
+      ' et un sens aigu de l’innovation. Ces qualités constituent pour nos clients le meilleur gage' +
+      ' d’un conseil nourri d’expériences variées, d’expertises éprouvées et d’une obsession affirmée ' +
+      'de la qualité du service offert.'
+    };
+
+  box2 = {
+    title: '3P CONSEIL Artisan de la performance',
+    description: 'Partenaire des conseils régionaux, provinciaux, préfectoraux, ' +
+    'et communaux dans la mise en œuvre des nouvelles lois organiques 111-14, 112-14, 113-14.'
+  };
+
+  box3 = {
+    title: 'Toujours des services de qualité à des prix raisonnables et compétitifs',
+    description: 'Inspiré par les principes du commerce équitable, 3PConseil démocratise l’accès à un ' +
+    'service de qualité assuré par des experts du premier ordre.'
+  };
 
   constructor() { }
 
--- src/app/who-r/who-r.component.ts
+++ src/app/who-r/who-r.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhoRComponent implements OnInit {
 
+  title = 'Qui sommes nous';
+  description = '3P Conseil est un cabinet d’audit et de conseil créé en Août 2011.' +
+    ' Il a pour mission principale de réaliser des missions d’audit et de conseil au profit de ' +
+    'ses clients, et ce dans différents domaines.\n' +
+    'Ce qui fait la force de 3P Conseil, c’est surtout son capital humain, composé de compétences ayant ' +
+    'un bon niveau d’expertise, une grande intégrité, et un sens aigu de l’innovation. Ces qualités ' +
+    'constituent pour nos clients le meilleur gage d’un conseil nourri d’expériences variées, d’expertises' +
+    ' éprouvées et d’une obsession affirmée de la qualité du service offert.';
+
   constructor() { }
 
   ngOnInit() {
