1f9fd27f0f1d0b3a5f9bfa211e7838c6210841af
hafsasam <34801393+hafsasam@users.noreply.github.com>
2018-07-15 14:52:54 +0100

    added navigation links
    up, down and top

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,5 +13,16 @@ export class NavbarComponent {
     this.show = !this.show;
   }
 
+  goDown() {
+    window.scrollBy(0, 650);
+  }
+
+  goUp() {
+    window.scrollBy(0, -650);
+  }
+  goTop() {
+    window.scrollTo(0, 0);
+  }
+
   constructor() { }
 }
