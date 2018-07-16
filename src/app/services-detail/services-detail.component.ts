f432d6a5e06afd95e13e0f24e991f2d57375fd6a
hafsasam <34801393+hafsasam@users.noreply.github.com>
2018-07-16 23:18:54 +0100

    added content and bare min styling

--- src/app/services-detail/services-detail.component.ts
+++ src/app/services-detail/services-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Service } from '../services/service';
+import { Services3pService } from '../service/services3p.service';
+
 @Component({
   selector: 'app-services-detail',
   templateUrl: './services-detail.component.html',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesDetailComponent implements OnInit {
 
-  constructor() { }
+  services: Service[];
+
+  constructor( private ser3p: Services3pService ) { }
 
   ngOnInit() {
+    this.ser3p.getServices3p().subscribe(
+      services => this.services = services
+    );
   }
 
 }
