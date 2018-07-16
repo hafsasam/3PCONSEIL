d83040fc08391db65f2e5ed39a60ff9d78ca6d85
hafsasam <34801393+hafsasam@users.noreply.github.com>
2018-07-16 15:59:59 +0100

    changed photos

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -11,19 +11,22 @@ export class PresentationComponent implements OnInit {
       description: 'composé de compétences ayant un bon niveau d’expertise, une grande intégrité,' +
       ' et un sens aigu de l’innovation. Ces qualités constituent pour nos clients le meilleur gage' +
       ' d’un conseil nourri d’expériences variées, d’expertises éprouvées et d’une obsession affirmée ' +
-      'de la qualité du service offert.'
+      'de la qualité du service offert.',
+      img: '../../assets/img/2.png'
     };
 
   box2 = {
     title: '3P CONSEIL Artisan de la performance',
     description: 'Partenaire des conseils régionaux, provinciaux, préfectoraux, ' +
-    'et communaux dans la mise en œuvre des nouvelles lois organiques 111-14, 112-14, 113-14.'
+    'et communaux dans la mise en œuvre des nouvelles lois organiques 111-14, 112-14, 113-14.',
+    img: '../../assets/img/1.png'
   };
 
   box3 = {
     title: 'Toujours des services de qualité à des prix raisonnables et compétitifs',
     description: 'Inspiré par les principes du commerce équitable, 3PConseil démocratise l’accès à un ' +
-    'service de qualité assuré par des experts du premier ordre.'
+    'service de qualité assuré par des experts du premier ordre.',
+    img: '../../assets/img/3.png'
   };
 
   constructor() { }
