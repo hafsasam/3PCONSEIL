d560ec885405f4a6a368bf57cff161ff0a8300b6
hafsasam <34801393+hafsasam@users.noreply.github.com>
2018-07-17 15:57:46 +0100

    added animation

--- src/app/services-detail/services-detail.component.ts
+++ src/app/services-detail/services-detail.component.ts
@@ -3,10 +3,19 @@ import { Component, OnInit } from '@angular/core';
 import { Service } from '../services/service';
 import { Services3pService } from '../service/services3p.service';
 
+import { flyInOut } from '../animations';
+
 @Component({
   selector: 'app-services-detail',
   templateUrl: './services-detail.component.html',
-  styleUrls: ['./services-detail.component.css']
+  styleUrls: ['./services-detail.component.css'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 export class ServicesDetailComponent implements OnInit {
 
